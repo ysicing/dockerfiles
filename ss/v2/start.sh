238e99c5abab43873e22aaac53ff10d504987eae
ysicing zheng <ysbot@ysicing.net>
2018-11-11 06:33:18 +0800

    [REV] update ss

---
+++ ss/v2/start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+[ -z "$PORT" ] && PORT="110"
+[ -z "$PASS" ] && PORT="12345678"
+
+cat > /ss/gfw <<EOF
+{
+    "server": "0.0.0.0",
+    "server_port": "${PORT}",
+    "manager-address": "127.0.0.1:6666",
+    "local_address": "127.0.0.1",
+    "local_port": 1080,
+    "password": "${PASS}",
+    "timeout": 300,
+    "workers": 4,
+    "method": "salsa20",
+    "fast_open": false
+}
+
+EOF
+
+if [[ "$1" == "bash" ]];then
+  exec /bin/bash
+else
+  exec ssserver -c /ss/gfw.json
+fi
