0f2669543a4dc13456f7ea531581f00ee8aa5f81
ysicing <i@ysicing.me>
2019-11-09 11:09:55 +0800

    feat(caddy): add caddy

--- build.sh
+++ build.sh
@@ -2,9 +2,10 @@
 
 set -e
 
-images=$(ls -al | grep "drwxr"  | grep -v "\." | awk '{print $NF}' | tr '\n' ' ')
+images=$(ls -al | grep "drwxr"  | grep -v "\." | awk '{print $NF}' | grep -v "cachet" | tr '\n' ' ')
 for image in ${images[@]}
 do
+
     docker build -t ysicing/${image} ${image}
     docker push ysicing/${image}
 done
---
+++ caddy/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Caddy</title>
+<style>
+    body {
+        text-align: center;
+        font-family: Tahoma, Geneva, Verdana, sans-serif;
+    }
+</style>
+</head>
+<body>
+<h1>Caddy web server.</h1>
+<p>If you see PHP info below, Caddy with PHP container works.</p>
+
+<?php 
+    phpinfo() 
+?>
+</body>
+</html>
