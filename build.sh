2170de9162d1e73c32b41f2dc3df90551b81af5e
ysicing <i@ysicing.me>
2020-07-04 19:36:48 +0800

    fix: 移除gops ci构建镜像失败

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e
 images=$(ls -al | grep "drwxr"  | grep -v "\." | awk '{print $NF}' | grep -v "cachet" | tr '\n' ' ')
 for image in ${images[@]}
 do
-
+    echo "start build ${image}"
     docker build --no-cache -t ysicing/${image} ${image}
     docker push ysicing/${image}
 done
