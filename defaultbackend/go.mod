19331947d2673cdfddf625c582aff6e9da4bb812
ysicing <i@ysicing.me>
2020-11-13 18:40:52 +0800

    feat: add prom

--- defaultbackend/go.mod
+++ defaultbackend/go.mod
@@ -4,6 +4,6 @@ go 1.14
 
 require (
 	github.com/gin-gonic/gin v1.6.3
-	github.com/prometheus/client_golang v1.7.1
-	github.com/ysicing/ginmid v0.2.1
+	github.com/prometheus/client_golang v1.8.0
+	github.com/ysicing/ext v0.0.0-20201109070235-9c52c8bb9d30
 )
--- defaultbackend/main.go
+++ defaultbackend/main.go
@@ -6,17 +6,33 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/ysicing/ginmid"
+	"github.com/ysicing/ext/e"
+	"github.com/ysicing/ext/ginmid"
+	"github.com/ysicing/ext/logger"
 	"net/http"
 	"time"
 )
 
+var (
+	FoundDomainCount = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "default_backend_not_exist_domain",
+		Help: "访问ingress default backend",
+	})
+)
+
+func init()  {
+	logcfg := logger.Config{Simple: true, ConsoleOnly: true}
+	logger.InitLogger(&logcfg)
+	prometheus.MustRegister(FoundDomainCount)
+}
+
 func main() {
-
+	gin.SetMode(gin.DebugMode)
+	gin.DisableConsoleColor()
 	r := gin.New()
-
-	r.Use(mid.RequestID(), mid.PromMiddleware(nil))
+	r.Use(ginmid.RequestID(), ginmid.Log())
 
 	// Example ping request.
 	r.GET("/healthz", func(c *gin.Context) {
@@ -25,11 +41,20 @@ func main() {
 
 	// Example / request.
 	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "default backend. \n\nid: "+mid.GetRequestID(c))
+		// c.String(http.StatusOK, "default backend. \n\nid: "+mid.GetRequestID(c))
+		FoundDomainCount.Inc()
+		c.JSON(404, e.Error(10404, map[string]interface{}{
+			"message": "default backend",
+			"id": ginmid.GetRequestID(c),
+			"method": c.Request.Method,
+			"url": c.Request.Host,
+			"client": c.ClientIP(),
+			"ua": c.Request.UserAgent(),
+		}))
 	})
 
 	// Example /metrics
-	r.GET("/metrics", mid.PromHandler(promhttp.Handler()))
+	r.GET("/metrics", gin.WrapH(promhttp.Handler()))
 
 	// Listen and Server in 0.0.0.0:8080
 	r.Run(":8080")
