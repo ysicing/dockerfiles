6ebc57440ceb7872a9a99d4e8146894736572ed8
ysicing <i@ysicing.me>
2020-12-29 11:13:30 +0800

    feat: update code
    Signed-off-by: ysicing <i@ysicing.me>

--- build.sh
+++ build.sh
@@ -11,12 +11,12 @@ do
     cp -a ./upx ${image}/upx
     chmod +x ${image}/upx
     cat ${image}/Dockerfile | grep FROM | awk '{print $2}' | xargs -I {} docker pull {}
-    docker build --no-cache -t ysicing/${image} ${image}
+    docker build --no-cache --pull -t ysicing/${image} ${image}
     [ ! -z $mirrors ] && (
         docker tag ysicing/${image} registry.cn-beijing.aliyuncs.com/k7scn/${image}
         docker push registry.cn-beijing.aliyuncs.com/k7scn/${image}
     )
     docker push ysicing/${image}
-    curl -s https://cr.hk2.godu.dev/pull\?name="ysicing/${image}"
+    # curl -s https://cr.hk2.godu.dev/pull\?name="ysicing/${image}"
     rm -rf ${image}/upx
 done
