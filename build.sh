d753d6b148aba31a9dfb2c23a8ec40f61829f7d4
ysicing <i@ysicing.me>
2023-07-10 14:17:36 +0800

    fix(typo): fix ci typo
    fix ci typo
    
    Signed-off-by: ysicing <i@ysicing.me>

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ for image in ${images[@]}
 do
     echo "start build ${image}"
     cat ${image}/Dockerfile | grep FROM | awk '{print $2}' | xargs -I {} docker pull {}
-    docker buildx build --pull --psuh -t ysicing/${image} -t ghcr.io/ysicing/${image} ${image}
+    docker buildx build --pull --push -t ysicing/${image} -t ghcr.io/ysicing/${image} ${image}
     curl -s https://cr.hk1.godu.dev/pull\?image="ysicing/${image}"
 done
