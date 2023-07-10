60c09b08cc11f6403aeac962d940c9d5a82521b4
ysicing <i@ysicing.me>
2023-07-10 14:16:47 +0800

    feat(update): update
    update
    
    Signed-off-by: ysicing <i@ysicing.me>

--- build.sh
+++ build.sh
@@ -2,21 +2,11 @@
 
 set -e
 
-mirrors=$1
-
 images=$(ls -al | grep "drwxr"  | grep -v "\." | awk '{print $NF}' | tr '\n' ' ')
 for image in ${images[@]}
 do
     echo "start build ${image}"
     cat ${image}/Dockerfile | grep FROM | awk '{print $2}' | xargs -I {} docker pull {}
-    docker build --no-cache --pull -t ysicing/${image} ${image}
-    [ ! -z $mirrors ] && (
-        docker tag ysicing/${image} ghcr.io/ysicing/${image}
-        docker push ghcr.io/ysicing/${image}
-    )
-    docker push ysicing/${image}
+    docker buildx build --pull --psuh -t ysicing/${image} -t ghcr.io/ysicing/${image} ${image}
     curl -s https://cr.hk1.godu.dev/pull\?image="ysicing/${image}"
-    # rm -rf ${image}/upx
 done
-
-docker run --rm --name kubectl ghcr.io/ysicing/kubectl:latest
